937b09d2e63539d895672dead453cfb2151ed90d
Baraker42 <hakl6@seznam.cz>
2021-11-04 16:49:26 +0100

    možnost vypsání jídel pro konkrétní den

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import mysql.connector
 from flask_restful import Api, Resource, reqparse
 from secret import Password
 import json
-import os
 
 #There you have to define your connection with MYSQL
 try:
@@ -11,7 +10,7 @@ try:
     host="localhost",
     user="root",
     passwd=Password.passwrd,
-    database="Restauratitos"
+    database="Restaurants"
     )
 
 except:
@@ -25,14 +24,16 @@ app = Flask(__name__)
 api = Api(app)
 my_cursor = db.cursor()
 
+#Creating tables in database if not already created
 try:
-    my_cursor.execute("CREATE DATABASE IF NOT EXISTS Restauratitos")
+    my_cursor.execute("CREATE DATABASE IF NOT EXISTS Restaurants")
     my_cursor.execute("CREATE TABLE IF NOT EXISTS Restaurants(id int PRIMARY KEY AUTO_INCREMENT,name VARCHAR(255) NOT NULL,contact VARCHAR(255) NOT NULL,opening_hours VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL)")
     my_cursor.execute("CREATE TABLE IF NOT EXISTS Meals(id int PRIMARY KEY AUTO_INCREMENT, rest_id int NOT NULL, FOREIGN KEY (rest_id) REFERENCES Restaurants(id), name VARCHAR(255) NOT NULL, day VARCHAR(10) NOT NULL, price FLOAT(10) NOT NULL)")
 except:
     print("Database you try connect, doesn't exist.")
     quit()
 
+#Prepare getting arguments from used URL
 restaurant_put_args = reqparse.RequestParser()
 restaurant_put_args.add_argument("name", type=str, help="Name of the restaurant is required", required=True)
 restaurant_put_args.add_argument("contact", type=str, help="Contact of the restaurant is required", required=True)
@@ -55,6 +56,7 @@ meal_update_args.add_argument("name", type=str, help="Name of the meal")
 meal_update_args.add_argument("day", type=str, help="Day when meal is serve")
 meal_update_args.add_argument("price", type=float, help="Price of the meal")
 
+#Class for creating new restaurant(post) or get list of all IDs and names restaurants(get)
 class AllRestaurants(Resource):
     def post(self):
         args = restaurant_put_args.parse_args()
@@ -63,18 +65,18 @@ class AllRestaurants(Resource):
         return json.dumps({"name":args["name"], "contact":args["contact"], "opening_hours":args["opening_hours"], "address":args["address"]},ensure_ascii=False)
 
     def get(self):
-        my_cursor.execute("SELECT id,name FROM Restaurants")
-        row=my_cursor.fetchone()
+        my_cursor.execute("SELECT * FROM Restaurants")
+        row=my_cursor.fetchall()
         if row is None:
             abort(404, description="There are no any restaurant yet")
+        #my_cursor.execute("SELECT name FROM Restaurants")
         restaurant_list=[]
-        for i in my_cursor:
+        for i in row:
             restaurant={"id":i[0], "name":i[1]}
             restaurant_list.append(restaurant)
-        if len(restaurant_list)==0:
-            abort(404, description="There are no any restaurant yet")
         return json.dumps(restaurant_list,ensure_ascii=False)
 
+#Class for getting details about specific restaurant(get), updating information(put) and delete restaurant(delete)
 class Restaurant(Resource):
     def get(self, restaurant_id):
         my_cursor.execute("SELECT * FROM Restaurants WHERE id={0}".format(restaurant_id))
@@ -110,6 +112,7 @@ class Restaurant(Resource):
         row = my_cursor.fetchone()
         if row is None:
             abort(404, description="There is no restaurant with this ID")
+        #if restaurant allready contains some meals this command delete meals first
         my_cursor.execute("SELECT * FROM Meals WHERE rest_id={0}".format(restaurant_id))
         for i in my_cursor:
             meal_id=(i[1])
@@ -118,7 +121,7 @@ class Restaurant(Resource):
         my_cursor.execute("DELETE FROM Restaurants WHERE id={0}".format(restaurant_id))
         db.commit()
         return json.dumps({"message":"Restaurant was successfully deleted"})
-
+#Class for getting all menu of specific restaurant
 class RestaurantMenu(Resource):
     def get(self, restaurant_id):
         my_cursor.execute("SELECT * FROM Restaurants WHERE id={0}".format(restaurant_id))
@@ -134,6 +137,23 @@ class RestaurantMenu(Resource):
             abort(404, description="There are no meals in this restaurant yet")
         return json.dumps(restaurant_menu)
 
+#Class for get menu for specific day
+class RestaurantDaily(Resource):
+    def get(self, restaurant_id, day):
+        my_cursor.execute("SELECT * FROM Restaurants WHERE id={0}".format(restaurant_id))
+        row = my_cursor.fetchone()
+        if row is None:
+            abort(404, description="There is no restaurant wit this ID")
+        my_cursor.execute("SELECT * FROM Meals WHERE rest_id={0} AND day='{1}'".format(restaurant_id, day))
+        daily_menu=[]
+        for i in my_cursor:
+            meal={"id":i[0],"name":i[2],"day":i[3],"price":i[4]}
+            daily_menu.append(meal)
+        if len(daily_menu)==0:
+            abort(404, description="There are no meals of this day in restaurant yet")
+        return json.dumps(daily_menu)
+
+#Class for creating new meal object
 class NewMeal(Resource):
     def post(self,restaurant_id):
         try:
@@ -144,6 +164,7 @@ class NewMeal(Resource):
         except:
             return json.dumps({"message":"There is no such restaurant with this id"})
 
+#Class for updating meal(put) or delete it(delete)
 class UpdateMeal(Resource):
     def put(self,meal_id):
         args = meal_update_args.parse_args()
@@ -172,12 +193,13 @@ class UpdateMeal(Resource):
         return json.dumps({"message":"Meal was deleted successfully"})
 
 
-api.add_resource(AllRestaurants,"/restaurants/")
-api.add_resource(Restaurant,"/restaurant/<int:restaurant_id>")
-api.add_resource(RestaurantMenu,"/restaurant/<int:restaurant_id>/menu")
+api.add_resource(AllRestaurants,"/restaurants/") #create new restaurant(post)/get list(get)
+api.add_resource(Restaurant,"/restaurant/<int:restaurant_id>")#GET detail/PUT change of restaurant/DELETE restaurant
+api.add_resource(RestaurantMenu,"/restaurant/<int:restaurant_id>/menu")#GET all meal of specific restaurant of whole week
+api.add_resource(RestaurantDaily,"/restaurant/<int:restaurant_id>/<string:day>")#GET menu for specific day
 
-api.add_resource(NewMeal,"/<int:restaurant_id>/new_meal")
-api.add_resource(UpdateMeal, "/meal/<int:meal_id>")
+api.add_resource(NewMeal,"/<int:restaurant_id>/new_meal")#create new meal
+api.add_resource(UpdateMeal, "/meal/<int:meal_id>")#PUT change of meal/DELETE meal
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,22 +2,24 @@ import requests
 import json
 
 BASE = "http://127.0.0.1:5000/"
-response = requests.post(BASE + "restaurants/",{"name":"Vendys","contact":"7388124","opening_hours":"10-24","address":"Formanská 56"})
+#response = requests.post(BASE + "restaurants/",{"name":"Lokááál","contact":"7388124","opening_hours":"10-24","address":"Formanská 56"})
 #response = requests.put(BASE + "restaurant/1",{"name":"lelek"})
 #print(response.json())
 #response = requests.post(BASE + "1/new_meal",{"name":"svickova","day":"pondeli","price":"200.00"})
-#response = requests.post(BASE + "1/new_meal",{"name":"gulash","day":"pondeli","price":"200.00"})
-#response = requests.post(BASE + "1/new_meal",{"name":"koprovka","day":"pondeli","price":"200.00"})
-#response = requests.delete(BASE + "restaurant/10")
+#response = requests.post(BASE + "1/new_meal",{"name":"gulash","day":"utery","price":"200.00"})
+#response = requests.post(BASE + "1/new_meal",{"name":"koprovka","day":"streda","price":"200.00"})
+#response = requests.post(BASE + "1/new_meal",{"name":"rajska","day":"ctvrtek","price":"200.00"})
+#response = requests.post(BASE + "1/new_meal",{"name":"spagety","day":"patek","price":"200.00"})
+#response = requests.delete(BASE + "restaurant/1")
 #print(response.json())
 #response = requests.get(BASE + "meals")
 #response = requests.put(BASE + "/meal/5",{"price":"150.00"})
 #response = requests.delete(BASE + "meal/1")
-#response = requests.get(BASE + "/restaurant/1/menu")
+#response = requests.get(BASE + "/restaurant/1/streda")
 #print(response.json())
 
 #response = requests.put(BASE + "/meal/1",{"name":"zelnacka"})
-#response = requests.get(BASE + "restaurant/15")
+response = requests.get(BASE + "restaurants/")
 
 #response = requests.get(BASE + "1/menu")
 #response = requests.delete(BASE + "/meal/5")
